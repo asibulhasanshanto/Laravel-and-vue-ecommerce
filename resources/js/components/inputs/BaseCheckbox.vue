89b7cf694c30c03a8d60e8a74b52354ec8814038
Md Asibul Hasan Shanto <48201478+asibulhasanshanto@users.noreply.github.com>
2022-10-25 20:48:55 +0600

    base input and checkbox created

---
+++ resources/js/components/inputs/BaseCheckbox.vue
@@ -0,0 +1,23 @@
+<template>
+    <input
+        type="checkbox"
+        :checked="modelValue"
+        @change="$emit('update:modelValue', $event.target.checked)"
+        class="field"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        label: {
+            type: String,
+            default: "",
+        },
+        modelValue: {
+            type: Boolean,
+            default: false,
+        },
+    },
+};
+</script>
